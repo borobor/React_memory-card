c5ac2a0ff68850cbad02e8a6a39ba510a90f9666
BornaLap <bornajurisa@gmailcom>
2022-11-22 19:04:02 +0100

    Rearrange assets for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import Cards from "./Cards";
 import "./App.css";
 
 const cardSet = [
-  { src: "./src/assets/img/card1.png", text: "Finn the Human", id: 1 },
-  { src: "./src/assets/img/card2.png", text: "Jake the Dog", id: 2 },
-  { src: "./src/assets/img/card3.png", text: "Princess Bublegum", id: 3 },
-  { src: "./src/assets/img/card4.png", text: "BMO", id: 4 },
-  { src: "./src/assets/img/card5.png", text: "Ice King", id: 5 },
-  { src: "./src/assets/img/card6.png", text: "Lumpy Space Princess", id: 6 },
-  { src: "./src/assets/img/card7.png", text: "Earl of Lemongrab", id: 7 },
-  { src: "./src/assets/img/card8.png", text: "Marceline the Vampire Queen", id: 8 },
-  { src: "./src/assets/img/card9.png", text: "Peppermint Butler", id: 9 },
-  { src: "./src/assets/img/card10.png", text: "Gunter", id: 10 },
+  { src: "/card1.png", text: "Finn the Human", id: 1 },
+  { src: "/card2.png", text: "Jake the Dog", id: 2 },
+  { src: "/card3.png", text: "Princess Bublegum", id: 3 },
+  { src: "/card4.png", text: "BMO", id: 4 },
+  { src: "/card5.png", text: "Ice King", id: 5 },
+  { src: "/card6.png", text: "Lumpy Space Princess", id: 6 },
+  { src: "/card7.png", text: "Earl of Lemongrab", id: 7 },
+  { src: "/card8.png", text: "Marceline the Vampire Queen", id: 8 },
+  { src: "/card9.png", text: "Peppermint Butler", id: 9 },
+  { src: "/card10.png", text: "Gunter", id: 10 },
 ];
 
 function App() {
@@ -54,7 +54,7 @@ function App() {
     <div className="content">
       <header>
 				<div className="header-logo">
-					<img src="./src/assets/img/game-logo.png" alt="Adventure Time logo" />
+					<img src="game-logo.png" alt="Adventure Time logo" />
 					<p>Memory game</p>
 				</div>
 				<div className="header-score">
